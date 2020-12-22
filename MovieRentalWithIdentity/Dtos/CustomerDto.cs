8718e44db7c5d5416e683fe43e08346abce7253a
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2020-12-22 21:14:07 +0530

    Section 6 - Exercise : Create WebAPI for Movies

---
+++ MovieRentalWithIdentity/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using MovieRentalWithIdentity.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalWithIdentity.Dtos
+{
+    public class CustomerDto
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter Customer's Name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeID { get; set; }
+        
+        //[Display(Name = "Birthdate")]
+        //[Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
---
+++ MovieRentalWithIdentity/Dtos/MovieDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRentalWithIdentity.Dtos
+{
+    public class MovieDto
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreID { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
