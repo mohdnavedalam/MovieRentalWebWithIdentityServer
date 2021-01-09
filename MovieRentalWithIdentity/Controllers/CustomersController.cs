53ed57cc01994f61c53a9e58e929b0f6eca338e9
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-01-09 17:39:16 +0530

    Lecture - 105

--- MovieRentalWithIdentity/Controllers/CustomersController.cs
+++ MovieRentalWithIdentity/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
@@ -71,6 +72,11 @@ namespace MovieRentalWithIdentity.Controllers
         
         public ActionResult Index()
         {
+            if (MemoryCache.Default["Genres"] == null)
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+
             var customers = _context.Customers.ToList();
             return View(customers);
         }
