e325fa41db44a13442ec094d03a0227fe9a8ac3b
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-01-15 23:53:36 +0530

    Lecture 115

---
+++ MovieRentalWithIdentity/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,18 @@
+﻿using MovieRentalWithIdentity.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace MovieRentalWithIdentity.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieRentalWithIdentity/Dtos/NewRentalDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentalWithIdentity.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerID { get; set; }
+        public List<int> MovieIDs { get; set; }
+    }
+}
