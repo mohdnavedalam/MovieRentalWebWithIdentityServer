e6f5d2e1506e6d215bc7dd3add819a1b1c2feb8b
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2021-01-16 00:18:28 +0530

    Lecture 117

--- MovieRentalWithIdentity/Controllers/Api/NewRentalsController.cs
+++ MovieRentalWithIdentity/Controllers/Api/NewRentalsController.cs
@@ -1,4 +1,5 @@
 ﻿using MovieRentalWithIdentity.Dtos;
+using MovieRentalWithIdentity.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,40 @@ namespace MovieRentalWithIdentity.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+        ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.ID == newRental.CustomerID);
+
+            //var customer = _context.Customers.SingleOrDefault(c => c.ID == newRental.CustomerID);
+            //if (customer == null)
+            //    return BadRequest("Invalid Customer ID");
+
+            var movies = _context.Movies.Where(m => newRental.MovieIDs.Contains(m.ID));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);                
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+
         }
     }
 }
